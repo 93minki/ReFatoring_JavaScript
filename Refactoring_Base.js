e3802d6549533423da39a548289b20b4f8e28892
Minki <alsrl0023@naver.com>
2020-05-02 13:42:54 +0900

    ReF_1

--- Refactoring_Base.js
+++ Refactoring_Base.js
@@ -1,40 +1,45 @@
 function statement(invoice, plays){
     let totalAmount = 0;
     let volumeCredits = 0;
-    let result = '청구 내역 (고객명: ${invoice.customer})\n';
+    let result = 'Billing History (Customer Name: ${invoice.customer})\n';
     const format = new Intl.NumberFormat("en-US",{style: "currency", currency: "USD", minimumFractionDigits: 2}).format;
 
     for(let perf of invoice.performances){
         const paly = plays[perf.playID];
-        let thisAmount = 0;
-
-        switch(play.type){
-            case "tragedy":
-                thisAmount = 40000;
-                if(perf.audience > 30){
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if(perf.audience > 20){
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error('알수 없는 장르 : ${play.type}');
-        }
+        let thisAmount = amountFor(perf, play);
 
         volumeCredits += Math.max(per.audience - 30, 0);
         if("comedy" === play.type){
             volumeCredits += Math.floor(per.audience / 5);
         }
 
-        result += ' ${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n;
+        result += ' ${play.name}: ${format(thisAmount/100)} (${perf.audience}seat)\n';
         totalAmount += thisAmount;
     }
-    result += '총액: ${format(totalAmount/100}\n';
-    result += '적립 포인트: ${volumeCredits}점\n';
+    result += 'Total Amount: ${format(totalAmount/100}\n';
+    result += 'Points Earned: ${volumeCredits}point\n';
     return result;
+
+    function amountFor(aPerformance, play){
+        let result = 0;
+
+        switch(play.type){
+            case "tragedy":
+                result = 40000;
+                if(aPerformance.audience > 30){
+                    thisAmount += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if(aPerformance.audience > 20){
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error('Unknown Genre : ${play.type}');
+        }
+        return result;
+    }
 }
